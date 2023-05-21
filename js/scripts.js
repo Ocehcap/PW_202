3023463b510be1d765e0ea16a41112b62ac7fd39
Ocehcap <pauloypacheco@hotmail.com>
2023-05-21 21:13:52 +0100

    atualizacao

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,58 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+new Vue({
+    el: '#app',
+    data: {
+      filtroTamanho: '',
+      filtroMarca: '',
+      filtroCor: '',
+      trocas: [
+        {
+          id: 1,
+          nome: 'Cristiano Ronaldo',
+          perfil: './images/PERFILCR7.jpg',
+          imagem: './images/IMG_4412.png',
+          tamanho: '40',
+          marca: 'Nike'
+        },
+        {
+          id: 2,
+          nome: 'Lionel Messi',
+          perfil: './images/PERFILMESSI.jpg',
+          imagem: './images/IMG_4412.png',
+          tamanho: '42',
+          marca: 'Adidas'
+        },
+        {
+          id: 3,
+          nome: 'Neymar Jr',
+          perfil: './images/PERFILNEYMAR.jpg',
+          imagem: './images/IMG_4412.png',
+          tamanho: '38',
+          marca: 'Puma'
+        }
+      ],
+      trocasFiltradas: []
+    },
+    methods: {
+      aplicarFiltros() {
+        let trocasFiltradas = this.trocas;
+  
+        if (this.filtroTamanho !== '') {
+          trocasFiltradas = trocasFiltradas.filter(troca => troca.tamanho === this.filtroTamanho);
+        }
+  
+        if (this.filtroMarca !== '') {
+          trocasFiltradas = trocasFiltradas.filter(troca => troca.marca === this.filtroMarca);
+        }
+  
+        if (this.filtroCor !== '') {
+          trocasFiltradas = trocasFiltradas.filter(troca => troca.cor === this.filtroCor);
+        }
+  
+        this.trocasFiltradas = trocasFiltradas;
+      }
+    },
+    created() {
+      this.aplicarFiltros();
+    }
+  });
+  
